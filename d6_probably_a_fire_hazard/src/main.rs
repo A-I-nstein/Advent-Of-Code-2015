3de0d7c392cd4899f60cd9bde6a815c23ae6892e
A-I-nstein <anuranjan.m.b@gmail.com>
2025-05-19 10:06:34 +0530

    Completed day 6 puzzle.

--- d6_probably_a_fire_hazard/src/main.rs
+++ d6_probably_a_fire_hazard/src/main.rs
@@ -5,12 +5,12 @@ use regex::Regex;
 #[derive(Debug)]
 struct LighCommands<'a> {
     command: &'a str,
-    start_coord: (u32, u32),
-    end_coord: (u32, u32),
+    start_coord: (usize, usize),
+    end_coord: (usize, usize),
 }
 
 fn read_input(input_file: &str) -> Result<String, io::Error> {
-    return fs::read_to_string(input_file);
+    fs::read_to_string(input_file)
 }
 
 fn get_command(line: &str) -> LighCommands {
@@ -46,18 +46,81 @@ fn get_command(line: &str) -> LighCommands {
     }
 }
 
-fn parse_input(puzzle_input: &String) -> Vec<LighCommands> {
+fn parse_input(puzzle_input: &str) -> Vec<LighCommands> {
     let mut puzzle_input_commands: Vec<LighCommands> = Vec::new();
     for line in puzzle_input.lines() {
         puzzle_input_commands.push(get_command(line));
     }
-    return puzzle_input_commands;
+    puzzle_input_commands
+}
+
+fn apply_command(light_grid: &mut [Vec<usize>], command: &LighCommands) {
+    for i in (command.start_coord.0)..(command.end_coord.0 + 1) {
+        for j in (command.start_coord.1)..(command.end_coord.1 + 1) {
+            if command.command == "off" {
+                light_grid[i][j] = 0;
+            } else if command.command == "on" {
+                light_grid[i][j] = 1;
+            } else if command.command == "toggle" {
+                if light_grid[i][j] == 0 {
+                    light_grid[i][j] = 1;
+                } else if light_grid[i][j] == 1 {
+                    light_grid[i][j] = 0;
+                }
+            }
+        }
+    }
+}
+
+fn apply_command_2(light_grid: &mut [Vec<usize>], command: &LighCommands) {
+    for i in (command.start_coord.0)..(command.end_coord.0 + 1) {
+        for j in (command.start_coord.1)..(command.end_coord.1 + 1) {
+            if command.command == "off" {
+                if light_grid[i][j] > 0 {
+                    light_grid[i][j] -= 1;
+                }
+            } else if command.command == "on" {
+                light_grid[i][j] += 1;
+            } else if command.command == "toggle" {
+                light_grid[i][j] += 2;
+            }
+        }
+    }
 }
 
 fn main() -> Result<(), String> {
     let puzzle_input =
         read_input("input.txt").map_err(|e| format!("Could not read input file: {}", e))?;
-    let puzzle_input_commands: Vec<LighCommands> = parse_input(&puzzle_input);
-    println!("{:?}", puzzle_input_commands);
+
+    let puzzle_input_commands: Vec<LighCommands> = parse_input(&puzzle_input[0..]);
+
+    let mut light_grid: Vec<Vec<usize>> = vec![vec![0; 1000]; 1000];
+    for command in &puzzle_input_commands {
+        apply_command(&mut light_grid, command);
+    }
+
+    let mut num_of_light_on: usize = 0;
+    for row in &light_grid {
+        for light_state in row {
+            if *light_state == 1 {
+                num_of_light_on += 1;
+            }
+        }
+    }
+    println!("Number of lights turned on: {}", num_of_light_on);
+
+    let mut light_grid: Vec<Vec<usize>> = vec![vec![0; 1000]; 1000];
+    for command in &puzzle_input_commands {
+        apply_command_2(&mut light_grid, command);
+    }
+
+    let mut total_brightness: usize = 0;
+    for row in &light_grid {
+        for brightness in row {
+            total_brightness += brightness;
+        }
+    }
+    println!("Total Brightness: {}", total_brightness);
+
     Ok(())
 }
